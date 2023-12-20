5cc6797254d71c0ecd3d6986c3b3be3bd5d49482
pixis1127 <yumi.kawasaki1127@gmail.com>
2023-12-20 21:42:46 +0900

    7章課題の提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,21 +1,15 @@
-let num = 6;
+let num = 20;
 
-//3の倍数
-if (num % 3 === 0 && num % 15!== 0) {
+
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
     console.log('3の倍数です');
 }
-
-//5の倍数
 else if (num % 5 === 0 && num % 15!== 0) {
     console.log('5の倍数です');
 }
-
-//3と5の倍数 
-else if (num % 3 === 0 && num % 5 === 0) {
-    console.log('3と5の倍数です');
-}
-
-//それ以外
 else {
     console.log(num);
 }

f34a0654dc590c54d66ce77562a8cbf7e7c18836
pixis1127 <yumi.kawasaki1127@gmail.com>
2023-12-27 11:04:29 +0900

    20章課題の提出

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('btn');
+
+button.addEventListener('click', () => {
+    const word =document.getElementById('text');
+    word.textContent ='ボタンがクリックされました';
+
+})
